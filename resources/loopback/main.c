a9eb5fb548e8cee3d773df1806e978bb0164d4ed
Yehowshua Immanuel <yehowshua@talosiilite.hsd1.ga.comcast.net>
2021-06-12 13:38:33 -0400

    loopback now working

--- resources/loopback/main.c
+++ resources/loopback/main.c
@@ -1,10 +1,12 @@
 # include "uart_lpc_serial.h"
 
-int main() {
+int main(int argc, char **argv) {
     uart_init_ppc();
+    const char my_char[1] = {'A'};
+    mp_hal_stdout_tx_strn(my_char, 1);
+
     while (1) {
-        char my_char[1] = {'A'};
-        mp_hal_stdout_tx_strn(my_char, 1);
     }
+
     return 0;
 }
