2f7e539c4967b159793ac5f7c427f8a8de83fb4b
Yehowshua Immanuel <yehowshuai@chipeleven.com>
2021-06-13 18:47:34 -0400

    slight restructuring

--- src/loadMemory.nim
+++ src/loadMemory.nim
@@ -1,27 +1,33 @@
-import argparse
 import os
 import sequtils
 import sugar
 import itertools
+import math
 
-proc loadMemory*() : seq[uint32] = 
+proc loadMemory*(mem_depth : int, firmware_filepath : string) : seq[uint32] = 
   var memory = newSeq[uint32]()
   
-  var p = newParser:
-    option("-f", "--firmware", help="firmware file for simulator to execute")
-  var opts = p.parse(commandLineParams())
-   
-  try:
-    assert opts.firmware != ""
+  # load firmware into memory seq
+  var memory_by_bytes = open(firmware_filepath).readAll.map(x => x.uint8)
+
+  # can the specified memory size(mem_depth) contain the firmware?
+  try: 
+    var mem_len = ceil(memory_by_bytes.len / 4).int
+    assert mem_len <= mem_depth
   except Exception as e:
-    echo p.help
-    quit(0)
+    echo e.msg
+    echo "mem_depth = ", mem_depth, " is too small to contain firmware from", firmware_filepath
+    quit(1)
   
-  var memory_by_bytes = open(opts.firmware).readAll.map(x => x.uint8)
+  # load mem
   for vec4bytes in memory_by_bytes.chunked(4):
     var vec4uint32 = vec4bytes.map(x => x.uint32)
     var mem_word = [vec4uint32[0], vec4uint32[1] shl 8, vec4uint32[2] shl 16, vec4uint32[3] shl 24]
     var folded = mem_word.foldl(a or b)
     memory.add folded
-  
+
+  # pad any remaining space in the memory seq with 0s
+  while memory.len < mem_depth:
+    memory.add(1)
+   
   return memory
--- src/nimulatorPPC.nim
+++ src/nimulatorPPC.nim
@@ -1,8 +1,39 @@
-import print
-import sequtils
 import sugar
 import strutils
+import argparse
 import loadMemory
 
-var memory = loadMemory()
+# temporary imports
+import print
+
+# some variables we need for setup
+var firmware_filepath : string
+var mem_depth : int
+
+# set up the command line argument parser
+var p = newParser:
+  option("-f", "--firmware", help="firmware file for simulator to execute")
+  option("-d", "--memory_depth", default=some("1024"), help="how many entries deep the simulated CPU memory should be")
+
+# check that necessary arguments were passed
+try:
+  # validate arguments
+  var opts = p.parse(commandLineParams())
+  assert opts.firmware != ""
+
+  # assign values to setup variables
+  firmware_filepath = opts.firmware
+  mem_depth = opts.memory_depth.parseInt
+
+except ShortCircuit as e:
+  echo p.help
+  quit(0)
+
+except Exception as e:
+  echo e.msg
+  echo p.help
+  quit(0)
+
+# go ahead and load memory
+var memory = loadMemory(mem_depth, firmware_filepath)
 print memory.map(x => x.BiggestInt.toBin(32))
