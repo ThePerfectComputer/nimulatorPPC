1b42f23684c1b39a43eca1653fcaa034c6f516ed
Yehowshua Immanuel <yehowshuai@chipeleven.com>
2021-06-14 20:34:27 -0400

    add passing assertions

--- src/nimulatorPPC.nim
+++ src/nimulatorPPC.nim
@@ -11,23 +11,12 @@ import config
 var cpu_ram : array[config.ram_size, uint32]
 cpu_ram.init(config.firmware)
 
-print "0x" & cpu_ram.readUint8(0x16, ENDIAN.LITTLE).BiggestInt.toHex(2)
-print "0x" & cpu_ram.readUint8(0x16, ENDIAN.BIG).BiggestInt.toHex(2)
-
-print "0x" & cpu_ram.readUint16(0x16, ENDIAN.LITTLE).BiggestInt.toHex(4)
-print "0x" & cpu_ram.readUint16(0x16, ENDIAN.BIG).BiggestInt.toHex(4)
-
-print "0x" & cpu_ram.readUint32(0x16, ENDIAN.LITTLE).BiggestInt.toHex(8)
-print "0x" & cpu_ram.readUint32(0x16, ENDIAN.BIG).BiggestInt.toHex(8)
-
-print "0x" & cpu_ram.readUint64(0x16, ENDIAN.LITTLE).BiggestInt.toHex(16)
-print "0x" & cpu_ram.readUint64(0x16, ENDIAN.BIG).BiggestInt.toHex(16)
-
-# "0x25"
-# "0x25"
-# "0x8825"
-# "0x2588"
-# "0x00018825"
-# "0x25880100"
-# "0x0000382100018825"
-# "0x2588010021380000"
+assert cpu_ram.readUint8(0x16, ENDIAN.LITTLE) == 0x25.uint64
+assert cpu_ram.readUint8(0x16, ENDIAN.BIG) == 0x25.uint64
+assert cpu_ram.readUint16(0x16, ENDIAN.LITTLE) == 0x8825.uint64
+assert cpu_ram.readUint16(0x16, ENDIAN.BIG) == 0x2588.uint64
+assert cpu_ram.readUint32(0x16, ENDIAN.LITTLE) == 0x18825.uint64
+assert cpu_ram.readUint32(0x16, ENDIAN.BIG) == 0x25880100.uint64
+assert cpu_ram.readUint64(0x16, ENDIAN.LITTLE) == 0x382100018825.uint64
+assert cpu_ram.readUint64(0x16, ENDIAN.BIG) == 0x2588010021380000.uint64
+echo "finished"
