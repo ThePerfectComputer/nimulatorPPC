f28769ba5066bb821b528571be7b8f4d874c3975
Yehowshua Immanuel <yehowshuai@chipeleven.com>
2021-06-23 16:33:26 -0400

    loads to UART now seem to be working

--- src/nimulatorPPC.nim
+++ src/nimulatorPPC.nim
@@ -3,6 +3,12 @@ from cpu/fetch import fetchInstruction
 from cpu/decoder import getOp
 from cpu/execute import executeOp
 
+# set little endian
+from cpu/regfiles import nil
+regfiles.MSR[0] = 1
+
+# print MSR[0]
+
 for cycle in 0..8:
   cpu_membus
   .fetchInstruction()
