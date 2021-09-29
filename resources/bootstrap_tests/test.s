9402c8f21846afcafb241d3d68420aeaca74e548
Yehowshua Immanuel <programmed4jesus@gmail.com>
2021-09-29 15:48:42 -0400

    proper print macro

--- resources/bootstrap_tests/test.s
+++ resources/bootstrap_tests/test.s
@@ -6,51 +6,58 @@
 # As of right now, this assembly file just prints
 # "Hello World!" to the UART of said POWER CPU.
 
+.set uart_base_address, 0xC0002000
+
+.macro print_line my_string:vararg
+prelude\@:
+  # get UART address
+  addi %r1, 0, 0
+  oris %r1, %r1, uart_base_address@h
+  ori  %r1, %r1, uart_base_address@l
+
+  # load string addresss
+  addi %r2, 0, 0
+  oris %r2, %r2, (msg\@)@h
+  ori  %r2, %r2, (msg\@)@l
+
+print_loop\@:
+  # load charater at pointer r2
+  lbz   %r3, 0(%r2)
+
+  # if null-character, exit routine
+  cmpdi %r3, 0
+  beq   exit\@
+
+  # else write character to UART
+  stb   %r3, 0(%r1)
+
+  # increment character pointer
+  addi  %r2, %r2, 1   
+  # continue printing
+  b     print_loop\@
+
+msg\@:
+  .asciz  "\my_string\r\n\()"
+  .balign 8
+
+exit\@:
+
+.endm
+
 set_divisor:
   # this is primarily intended for chiselwatt and
   # should have no effect on nimulatorPPC
-  lis   %r5, uart_address@h
-  ld    %r5, uart_address@l(%r5)
+  addi  %r5, 0, 0
+  oris  %r5, %r5, uart_base_address@h
+	ori   %r5, %r5, uart_base_address@l
+
   addi  %r1, 0, 26
   stb   %r1, 0x18(%r5)
 
 basic_routine:
-  # load string address
-  addis %r1,   0, msg@h
-  addi  %r1, %r1, msg@l
-  # call print_string routine
-  bl    print_string
+  # print out some messages
+  print_line Hello World
+  print_line Goodbye World
 
   # once print_string finishes, we stall here
   b     .
-
-print_string: # (%r1 : null terminated string address)
-  # %r3: UART_ADDRESS
-  lis   %r3, uart_address@h
-  ld    %r3, uart_address@l(%r3)
-
-print_loop:
-  # load 
-  lbz   %r4, 0(%r1)
-
-  # if null-character, jump to link register
-  cmpdi %r4, 0
-  beqlr
-
-  # else write character to UART
-  stb   %r4, 0(%r3)
-
-  # increment character pointer
-  addi  %r1, %r1, 1   
-  # continue printing
-  b     print_loop
-  
-
-msg:
-  .asciz  "Hello, world!\r\n"
-  .balign 8
-  .quad 
-
-.balign 8
-uart_address:
-  .quad 0xc0002000
