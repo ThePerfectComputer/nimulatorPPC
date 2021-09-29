cf2010ddfa49b040f2a9cbb5a780d95ea83b5b51
Yehowshua Immanuel <programmed4jesus@gmail.com>
2021-09-28 21:30:29 -0400

    more efficient with null terminated strings

--- resources/bootstrap_tests/test.s
+++ resources/bootstrap_tests/test.s
@@ -16,42 +16,40 @@ set_divisor:
 
 basic_routine:
   # load string address
-  addi  %r1,   0, msg@h
+  addis %r1,   0, msg@h
   addi  %r1, %r1, msg@l
-  li    %r2, msg_end
   # call print_string routine
   bl    print_string
 
   # once print_string finishes, we stall here
   b     .
-  
 
-print_string: # (%r1 : string-address, %r2 : string-end-address)
+print_string: # (%r1 : null terminated string address)
   # %r3: UART_ADDRESS
   lis   %r3, uart_address@h
   ld    %r3, uart_address@l(%r3)
 
 print_loop:
-  # load and write character to UART
-
+  # load 
   lbz   %r4, 0(%r1)
+
+  # if null-character, jump to link register
+  cmpdi %r4, 0
+  beqlr
+
+  # else write character to UART
   stb   %r4, 0(%r3)
 
-  # if end of string, jump to link register
-  cmp   0, 1, %r1, %r2 
-  bclr  0b01100, 2, 0
-
-  # else increment character pointer and
-  # continue printing
+  # increment character pointer
   addi  %r1, %r1, 1   
+  # continue printing
   b     print_loop
   
 
 msg:
-  .ascii "Hello, world!\r\n"
+  .asciz  "Hello, world!\r\n"
   .balign 8
   .quad 
-  msg_end = . - 1
 
 .balign 8
 uart_address:
